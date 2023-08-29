8ed6afa4e1d5251e018480eb86053b4717f2f886
Sreejit Bakshi <109298517+SreejitBakshi10@users.noreply.github.com>
2023-08-29 23:09:22 +0530

    Add files via upload

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,13 @@
+(function (window) 
+{
+  var speakWord = "Hello";
+  var helloSpeaker = 
+  {
+    speak: function (name) 
+    {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.helloSpeaker = helloSpeaker;
+})(window);
+
