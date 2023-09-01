188dfe4de5bb646f4b4f39c20bb3be8d385ee1c7
Sreejit Bakshi <109298517+SreejitBakshi10@users.noreply.github.com>
2023-09-02 03:28:25 +0530

    Add files via upload

---
+++ Industrial Training/Form/script.js
@@ -0,0 +1,16 @@
+const register = document.getElementById('register');
+const login = document.getElementById('login');
+const signLink = document.getElementById('sign');
+const logLink = document.getElementById('log');
+
+logLink.addEventListener("click", function(){
+    login.style.opacity = 1;
+    register.style.opacity = 0;
+    document.title = 'Login';
+});
+
+signLink.addEventListener("click", function(){
+    login.style.opacity = 0;
+    register.style.opacity = 1;
+    document.title = 'Register';
+});
